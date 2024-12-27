520f795eb48ec4491a89db70f35b6bfcd2f67161
Funda Kayatürk <kayaturkfunda@gmail.com>
2024-12-27 11:57:08 +0300

    update

--- BLL/obj/Debug/net8.0/BLL.AssemblyInfo.cs
+++ BLL/obj/Debug/net8.0/BLL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c73df0a3b1364ba00dd79dba53d82ce2da28f90e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb9d5ba2a44691e00215f0e36a67d24101eead86")]
 [assembly: System.Reflection.AssemblyProductAttribute("BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
